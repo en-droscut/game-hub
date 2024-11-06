fe8fe5edde10069fec3fa61b98b76aec1d67c5de
Dragoș Roșcuț <dragos.roscut@endava.com>
2024-11-06 15:41:40 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //devices that are wider than 1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
